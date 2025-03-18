47a6c454ff698ce05ee5d4359ce322f1b295d15a
liuzhi <1306086303@qq.com>
2025-03-18 16:04:53 +0800

    feat: 新增用户注册，忘记密码，系统全局字体

--- src/config/setting.ts
+++ src/config/setting.ts
@@ -2,7 +2,7 @@
  * @Author: liuzhi 1306086303@qq.com
  * @Date: 2025-03-12 11:00:23
  * @LastEditors: liuzhi 1306086303@qq.com
- * @LastEditTime: 2025-03-17 15:55:40
+ * @LastEditTime: 2025-03-18 15:42:29
  * @FilePath: \continew-admin-ui\src\config\setting.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ export const defaultSettings: App.AppSettings = {
   layout: 'left',
   enableColorWeaknessMode: false,
   enableMourningMode: false,
-  fontFamily: 'PingFang SC',
+  fontFamily: 'Microsoft YaHei',
 }
 // 根据环境返回配置
 export const getSettings = (): App.AppSettings => {
